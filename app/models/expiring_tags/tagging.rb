8cd897eed66bba7872b42dd9277b1d97be59aeed
Wilson Silva <wilson.dsigns@gmail.com>
2014-12-12 13:11:59 +0000

    Disable the tangible and tag presence validation
    This validation is causing problems in the final app

--- app/models/expiring_tags/tagging.rb
+++ app/models/expiring_tags/tagging.rb
@@ -2,5 +2,5 @@ class ExpiringTags::Tagging < ActiveRecord::Base
   belongs_to :taggable, polymorphic: true
   belongs_to :tag, class_name: 'ExpiringTags::Tag'
 
-  validates :taggable, :tag, presence: true
+  # validates :taggable, :tag, presence: true
 end
