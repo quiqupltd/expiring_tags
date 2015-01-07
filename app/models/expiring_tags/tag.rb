cfc2980d1ac23c9ca8520f2f8fbec206d934006f
Wilson Silva <wilson.dsigns@gmail.com>
2015-01-07 13:45:43 +0000

    Add scope to filter out expired tags

--- app/models/expiring_tags/tag.rb
+++ app/models/expiring_tags/tag.rb
@@ -2,4 +2,7 @@ class ExpiringTags::Tag < ActiveRecord::Base
   has_many :taggings, class_name: 'ExpiringTags::Tagging', dependent: :destroy
 
   validates :tag, presence: true
+
+  # Equivalent to created_at + expires_in.minutes > Time.now
+  scope :active, -> { where("(tags.created_at + (tags.expires_in || 'minutes')::interval) > clock_timestamp()") }
 end
