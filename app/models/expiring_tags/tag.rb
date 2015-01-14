7707b1e529d54770fe76466db63b18f6eb9f9be3
Wilson Silva <wilson.dsigns@gmail.com>
2015-01-14 11:20:53 +0000

    Use hours as expiration's unit

--- app/models/expiring_tags/tag.rb
+++ app/models/expiring_tags/tag.rb
@@ -3,8 +3,8 @@ class ExpiringTags::Tag < ActiveRecord::Base
 
   validates :tag, presence: true
 
-  # Equivalent to created_at + expires_in.minutes > Time.now
-  scope :active, -> { where("(tags.created_at + (expires_in || 'minutes')::interval) >= clock_timestamp() OR expires_in IS NULL") }
+  # Equivalent to created_at + expires_in.hours > Time.now
+  scope :active, -> { where("(tags.created_at + (expires_in || 'hours')::interval) >= clock_timestamp() OR expires_in IS NULL") }
 
   default_scope -> { active }
 end
